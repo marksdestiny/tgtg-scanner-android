28e6321463d3e42c4609bc7405ad7f7ff621d65e
Markus <marksdestiny@hotmail.com>
2024-02-04 22:41:05 +0100

    Add constraints for the check bags worker

--- app/src/main/java/at/faymann/tgtgscanner/data/WorkManagerTgtgScannerRepository.kt
+++ app/src/main/java/at/faymann/tgtgscanner/data/WorkManagerTgtgScannerRepository.kt
@@ -3,7 +3,9 @@ package at.faymann.tgtgscanner.data
 import android.content.Context
 import android.util.Log
 import androidx.lifecycle.asFlow
+import androidx.work.Constraints
 import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import at.faymann.tgtgscanner.worker.CheckBagsWorker
@@ -32,7 +34,12 @@ class WorkManagerTgtgScannerRepository(
         //    ExistingPeriodicWorkPolicy.KEEP,
         //    worker
         //)
+        val constrains = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
         val worker = OneTimeWorkRequestBuilder<CheckBagsWorker>()
+            .setConstraints(constrains)
             .build()
         workManager.enqueueUniqueWork(
             CHECK_BAGS_WORK_NAME,
