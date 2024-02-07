78e58c544650a7031cfe1eb39c684cc244698b5a
Markus <marksdestiny@hotmail.com>
2024-02-07 16:28:59 +0100

    Update OkHttp3 library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     // JSON serialization dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
